30eace3beb2bd7564869958ee5b38a4d832a0101
leosch-coding <leoscholtz45@gmail.com>
2026-09-01 02:19:29 +0000

    Update main.c

--- Goal1/main.c
+++ Goal1/main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdbool.h>
 
 uint32_t tick_counter = 0;
 uint32_t offset = 0;
@@ -18,18 +19,17 @@ void counter(void) {
        if (on_or_off == 1) {
        
        *GPIOA_ODR &= 0b11111111111111111111111111011111;
-       
        on_or_off = 0;
        
-       return;
        } else {
        
        *GPIOA_ODR |= 0b00000000000000000000000000100000;
-
        on_or_off = 1;
 
-       }}
-    *TIM2_SR |= 0b11111111111111111111111111111110;
+       }
+    }
+
+    *TIM2_SR &= 0b11111111111111111111111111111110;
 } 
 
 void main(void) {
@@ -39,12 +39,12 @@ void main(void) {
     // Use OR to set bit 0 to 1, giving TIM2 a clock
     *RCC_APB1ENR |= 0b1;
 
-    volatile uint32_t *TIM2_PSC = (uint32_t *)0x40000024;
+    volatile uint32_t *TIM2_PSC = (uint32_t *)0x40000028;
   
     // Move 15 into TIM2_PSC
     *TIM2_PSC = 15;
 
-    volatile uint32_t *TIM2_ARR = (uint32_t *)0x40000028;
+    volatile uint32_t *TIM2_ARR = (uint32_t *)0x4000002c;
 
     // Set the number of times to tick until interrupt to 999 (basically making it tick 1000 times)
     *TIM2_ARR = 999;
@@ -55,18 +55,22 @@ void main(void) {
 
     volatile uint32_t *NVIC_ISER0 = (uint32_t *)0xE000E100;
 
-    *NVIC_ISER0 |= 0b0000100000000000000000000000000;
+    *NVIC_ISER0 |= (1 << 28);
 
     volatile uint32_t *RCC_AHB1ENR = (uint32_t *)0x40023830;
 
     *RCC_AHB1ENR |= 0b1;
   
-    *GPIOA_MODER &= 0b11111111100111111111111111111111;
-    *GPIOA_MODER |= 0b00000000001000000000000000000000; 
+    *GPIOA_MODER &= 0b11111111111111111111001111111111; 
+    *GPIOA_MODER |= 0b00000000000000000000010000000000;
 
     // We make a pointer to TIM2_CR1
     volatile uint32_t *TIM2_CR1 = (uint32_t *)0x40000000;
 
     // Use OR to set bit 0 to 1, enabling the counter
     *TIM2_CR1 |= 0b1;
+
+    while (true) {
+
+    }
 }
